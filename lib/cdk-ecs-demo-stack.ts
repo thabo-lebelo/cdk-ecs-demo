dc7af6f2289bb56ab2cda9d58a555cde0e4c7cf2
Thabo Lebelo <thabolebelo@icloud.com>
2022-03-17 00:11:24 +0200

    Code refactore

--- lib/cdk-ecs-demo-stack.ts
+++ lib/cdk-ecs-demo-stack.ts
@@ -3,14 +3,14 @@ import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkEcsDemoStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkEcsDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // example resource
+        // const queue = new sqs.Queue(this, 'CdkEcsDemoQueue', {
+        //   visibilityTimeout: cdk.Duration.seconds(300)
+        // });
+    }
 }
